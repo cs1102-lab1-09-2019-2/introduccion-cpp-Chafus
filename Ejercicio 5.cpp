b7edc5c07f22279ab98db8c393042862c2a46332
Chafus <54276667+Chafus@users.noreply.github.com>
2019-08-25 13:17:30 -0500

    Add files via upload

---
+++ Ejercicio 5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+
+int main() {
+    std::vector<int> a = {1, 2, 3, 4, 5};
+    for (auto i : a) {
+      cout<<i;
+        // Imprimir los numeros
+    }
+} 
