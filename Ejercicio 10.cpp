83f1ff0a53c6bb5ad3081c4254bae550e39a2103
Chafus <54276667+Chafus@users.noreply.github.com>
2019-08-25 18:14:52 -0500

    Add files via upload

---
+++ Ejercicio 10.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <vector>
+using std::string;
+using std::cout;
+
+int main() {
+    int a = 5;
+    //Declarar una cadena
+    string b;
+    //Inicializar la cadena b
+    b = "Esto es una cadena";
+    cout<<a<<"\n";
+    cout<<b<<"\n";
+    cout<<b[0]<<"\n";
+    cout<<b[4]<<"\n";
+
+    std::vector<int> numeros = {4, 8, 9, 5, 6, 7};
+    cout<<numeros[0]<<"\n";
+
+    float numero{5.16};
+    cout<<numero<<"\n";
+
+}
