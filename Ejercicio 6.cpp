2579cbda23bbd5228ba9ea9471f7661699e43fed
Chafus <54276667+Chafus@users.noreply.github.com>
2019-08-25 13:35:07 -0500

    Add files via upload

---
+++ Ejercicio 6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+
+int main() {
+    std::vector<std::vector<int>> v{{1, 2}, {3, 4}, {5, 6}};
+    for (auto a : v) {
+        for (auto i : a) {
+          cout<<i<<"\n";
+            // imprimir i
+        }
+        // salto de linea
+    }
+}
