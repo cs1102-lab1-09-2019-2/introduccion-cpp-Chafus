240b2a619c312caeed1517cb8042727bb7699d0b
Chafus <54276667+Chafus@users.noreply.github.com>
2019-08-25 21:21:01 -0500

    Add files via upload

---
+++ Ejercicio 13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+
+    float base, altura;
+    cout<<"Ingrese la base del triangulo: ";
+    cin>>base;
+    cout<<"Ingrese la altura del triangulo: ";
+    cin>>altura;
+    float area = //calcular el area
+    cout<<"El area del triangulo es: "<<area<<"\n";
+
+}
